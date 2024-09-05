0714d51febbe213d836e0cec93dc1c7566aaa835
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-05 19:30:41 +0500

    added css to serch and icon

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 import './Weather.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 function Weather() {
   return (
     <div className='weather'>
-        <div className="serch-bar">
-            <input type="text"  placeholder='search'/>
-            <img src="" alt="" />
-        </div>
-
+      <div className="serch-bar">
+        <input type="text" placeholder='search' />
+       
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='img' />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Weather
+export default Weather;
