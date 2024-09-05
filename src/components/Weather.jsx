492beb35c6476c8bb953d5c2ffd8c92147833644
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-05 18:47:46 +0500

    added icones

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './Weather.css'
 function Weather() {
   return (
-    <div>
-  <h1>Weather</h1>
+    <div className='weather'>
+        <div className="serch-bar">
+            <input type="text"  placeholder='search'/>
+            <img src="" alt="" />
+        </div>
+
     </div>
   )
 }
